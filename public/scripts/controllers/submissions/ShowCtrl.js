f0b0f7230a4ff4a9a6beec56ac867d88aa425c8d
James Yu <jj6yu@uwaterloo.ca>
2015-08-10 10:12:06 -0400

    editing and deleting comments

--- public/scripts/controllers/submissions/ShowCtrl.js
+++ public/scripts/controllers/submissions/ShowCtrl.js
@@ -3,7 +3,10 @@ angular.module('rosaApp')
 .controller('SubmissionShowCtrl', function($scope, $resource, $routeParams, $location) {
 
 	var Submission = $resource('/api/submissions/:id'),
-		Comments = $resource('/api/submissions/:sid/comments');
+		Comments = $resource('/api/submissions/:sid/comments'),
+		Comment = $resource('/api/comments/:id', null, {
+			'update': { method: 'PUT' }
+		});
 	
 	var loaded = function(submission) {
 			if (!submission._id) {
@@ -34,6 +37,44 @@ angular.module('rosaApp')
 				$scope.isCommenting = false;
 			})
 		};
+	};
+
+	$scope.editingId = null;
+	$scope.isEditing = null;
+
+	$scope.editComment = function(comment) {
+		$scope.isEditing = true;
+		if (comment.editText) {
+			Comment.update(
+					{ id: comment._id },
+					{ text: comment.editText },
+					function() {
+						comment.text = comment.editText;
+						$scope.editText = null;
+						$scope.editCommentFocus(null);
+						$scope.isEditing = false;
+					}, function(err) {
+						$scope.editCommentFocus(null);
+						$scope.isEditing = false;	
+					});
+		}
+	};
+
+	$scope.editCommentFocus = function(comment) {
+		$scope.editingId = comment && comment._id;
+		if ( comment ) {
+			comment.editText = comment.text;
+		}
+	};
+
+	$scope.deleteComment = function(comment) {
+		Comment.delete({ id: comment._id },
+				function() {
+					var idx = $scope.submission.comments.indexOf(comment);
+					if (idx > -1) {
+						$scope.submission.comments.splice(idx, 1);
+					}
+				});
 	};
 
 	$scope.delete = function() {
