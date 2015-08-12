4c520e8e7390600b751c5f12076d872c89843153
James Yu <jj6yu@uwaterloo.ca>
2015-08-12 10:26:27 -0400

    UI fixes

--- app/scripts/directives/rich-text.js
+++ app/scripts/directives/rich-text.js
@@ -15,10 +15,28 @@ function() {
 			scope.parse = function(text) {
 				// TODO: better formatting like links and stuff
 				return text
-						.replace('&', '&amp;')
-						.replace('<', '&lt;')
-						.replace('>', '&gt;')
-						.replace('\n', '<br>');
+						// sanitize
+						.replace(/&/g, '&amp;')
+						.replace(/</g, '&lt;')
+						.replace(/>/g, '&gt;')
+
+						// escape
+						.replace(/\\\\/g, '&#92;')
+						.replace(/\\\*/g, '&#42;')
+						.replace(/\\~/g, '&#126;')
+						.replace(/\\\[/g, '&#91;')
+						.replace(/\\\]/g, '&#93;')
+						.replace(/\\\(/g, '&#40;')
+						.replace(/\\\)/g, '&#41;')
+
+						// arbitrary markdown
+						.replace(/\*(.+?)\*/g, '<em>$1</em>')
+						.replace(/~~(.+?)~~/g, '<del>$1</del>')
+						// probably should add a nofollow here...
+						.replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2">$1</a>')
+						.replace(/((http:\/\/|https:\/\/)\S+)/g, '<a href="$1">$1</a>')
+
+						.split('\n').join('<br>');
 			}
 		}
 	};
